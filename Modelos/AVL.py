8ad23235acc2467317d6099135bbbe3e28686a6c
Juan José G. <juan.galeano60536@ucaldas.edu.co>
2026-03-12 20:50:47 -0500

    Agregación de caso LR, RL y clase inserción pública

--- Modelos/AVL.py
+++ Modelos/AVL.py
@@ -1,21 +1,34 @@
 from Modelos.BST import BST
 
+
 class AVL(BST):
     def __init__(self):
         self._root = None
 
     def getRoot(self):
         return self._root
-    
+
     def setRoot(self, root):
         self._root = root
+
+    # Método público para insertar
+    def insert(self, node):
+
+        # si el árbol está vacío se inserta como raiz y el padre sería None
+        if self._root is None:
+            self._root = node
+            node.setParent(None)
+
+        # si ya hay raíz se llama al método privado
+        else:
+            self.__insert(self._root, node)
 
     # Método recursivo para insertar un nodo cuando se tiene raiz en el árbol
     def __insert(self, currentRoot, node):
         if node.getValue() == currentRoot.getValue():
             print(f"El valor del nodo {node.getValue()} ya existe en el árbol.")
         else:
-        # se verifica si el valor a insertar es mayor que el actual raiz
+            # se verifica si el valor a insertar es mayor que el actual raiz
             if node.getValue() > currentRoot.getValue():
                 # se verifica si existe un hijo derecho
                 if currentRoot.getRightChild() is None:
@@ -50,30 +63,44 @@ class AVL(BST):
     def checkBalance(self, node):
         if node is None:
             raise Exception("EL nodo a balancear no es válido")
-        elif node != self.root:
-            self.__checkBalance(node)
+        self.__checkBalance(node)
 
     # Método recursivo para validar el balanceo de un árbol
     def __checkBalance(self, node):
+        # Se identifica el padre antes de hacer la rotación para evitar confusiones
+        parent = node.getParent()
         bf = self.getBalanceFactor(node)
-        if bf > 1 or bf < -1:
-        # se identifica el caso de desbalanceo (LL, RR, RL, LR)
+        if abs(bf) > 1:
+            # se identifica el caso de desbalanceo (LL, RR, RL, LR)
             bfCase = self.getBalanceCase(node, bf)
             match bfCase:
                 case "LL":
+                    # rotar el nodo de en medio hacia la derecha
                     self.__rotateRight(node)
 
                 case "RR":
+                    # rotar el nodo de en medio hacia la izquierda
                     self.__rotateLeft(node)
 
+                case "LR":
+                    # rotar el hijo izquierdo
+                    self.__rotateLeft(node.getLeftChild())
 
+                    # rotar el nodo desbalanceado
+                    self.__rotateRight(node)
+                case "RL":
+                    # rotar el hijo derecho
+                    self.__rotateRight(node.getRightChild())
+
+                    # rotar el nodo desbalanceado
+                    self.__rotateLeft(node)
+        # Se verifica que el padre no sea None y se procede a balancearlo si lo requiere
+        if parent is not None:
+            self.__checkBalance(parent)
         # Método para balancear un caso de desbalanceo LL
-        else:
-        # se verifica que el nodo actual no sea la raiz, y se invoca el chequeo de balanceo con su padre.
-        # cuando es la raiz se finaliza la evaluación
-            if node != self.root:
-        #if node.getParent() is not None:
-                self.__checkBalance(node.getParent())
+        # elif node != self.__root:
+        # if node.getParent() is not None:
+        # self.__checkBalance(node.getParent())
 
     # método para el giro simple a la derecha
     def __rotateRight(self, topNode):
@@ -93,21 +120,20 @@ class AVL(BST):
         # reacomodar al nodo padre del superior apuntando al de la mitad
         # verificar si el superior era la raiz
         if parentTopNode is None:
-            self.root = middleNode
+            self._root = middleNode
             middleNode.setParent(None)
         else:
             if parentTopNode.getLeftChild() == topNode:
                 parentTopNode.setLeftChild(middleNode)
             else:
                 parentTopNode.setRightChild(middleNode)
-        # sin importar si era hijo izq o derecho, se asigna ese padre del superior como padre del nodo de la mitad
+            # sin importar si era hijo izq o derecho, se asigna ese padre del superior como padre del nodo de la mitad
             middleNode.setParent(parentTopNode)
 
         # reasignar el hijo derecho del nodo de la mitad al nodo superior que ya bajó como hijo derecho del nodo de la mitad
         topNode.setLeftChild(rightChildOfMiddleNode)
         if rightChildOfMiddleNode is not None:
             rightChildOfMiddleNode.setParent(topNode)
-
 
     # método para el giro simple a la izquierda
     def __rotateLeft(self, topNode):
@@ -127,14 +153,14 @@ class AVL(BST):
         # reacomodar al nodo padre del superior apuntando al de la mitad
         # verificar si el superior era la raiz
         if parentTopNode is None:
-            self.root = middleNode
+            self._root = middleNode
             middleNode.setParent(None)
         else:
             if parentTopNode.getLeftChild() == topNode:
                 parentTopNode.setLeftChild(middleNode)
             else:
                 parentTopNode.setRightChild(middleNode)
-        # sin importar si era hijo izq o derecho, se asigna ese padre del superior como padre del nodo de la mitad
+            # sin importar si era hijo izq o derecho, se asigna ese padre del superior como padre del nodo de la mitad
             middleNode.setParent(parentTopNode)
 
         # reasignar el hijo izquierdo del nodo de la mitad al nodo superior que ya bajó como hijo izquierdo del nodo de la mitad
@@ -142,30 +168,28 @@ class AVL(BST):
         if leftChildOfMiddleNode is not None:
             leftChildOfMiddleNode.setParent(topNode)
 
-
     # método para identificar el caso de desbalanceo
     def getBalanceCase(self, node, bf):
         bfCase = ""
         # caso negativo, va por R
         if bf < -1:
             bfChild = self.getBalanceFactor(node.getRightChild())
-        # caso negativo, va por R
+            # caso negativo, va por R
             if bfChild < 0:
                 bfCase = "RR"
             else:
-            # caso positivo va por L
+                # caso positivo va por L
                 bfCase = "RL"
         # caso positivo L
         else:
             bfChild = self.getBalanceFactor(node.getLeftChild())
-        # caso positivo, va por L
+            # caso positivo, va por L
             if bfChild > 0:
                 bfCase = "LL"
             else:
-            # caso negativo va por R
+                # caso negativo va por RM
                 bfCase = "LR"
         return bfCase
-
 
     # Método para calcular el BF de un nodo
     def getBalanceFactor(self, node):
